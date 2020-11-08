3310333d473b363bf2db2508a8b7d4ff632c127a
ragnabt <rajhona.gabor@gmail.com>
2020-11-08 18:24:10 +0100

    working on css style

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
+
 def home_view(request):
     return render(request, 'home.html')
+
 
 def services_view(request):
     return render(request, 'services.html')
 
+
 def about_view(request):
     return render(request, 'about.html')
+
 
 def contact_view(request):
     return render(request, 'contact.html')
