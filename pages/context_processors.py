9aba253004d799d78f00aec3367b4f418d3b22f7
ragnabt <rajhona.gabor@gmail.com>
2020-11-11 12:45:10 +0100

    context_processors.py

---
+++ pages/context_processors.py
@@ -0,0 +1,19 @@
+
+
+def global_context(request):
+    return {
+        "site_name": "Stylus photography",
+        "site_subtitle": "Ha már unod az egyforma képeket...",
+        "footer": "valaki@valami.com"
+
+    }
+
+
+
+
+
+
+
+
+
+
--- pages/views.py
+++ pages/views.py
@@ -1,55 +1,57 @@
 from django.shortcuts import render
 
-site_name = "Stylus photography"
+# context = {
+#         "site_name": "Stylus photography",
+#         "site_subtitle": "Ha már unod az egyforma képeket..."
+#     }
 
 def home_view(request):
-    # database request
-    context = {
-        "site_name": site_name,
-        "site_subtitle": "Ha már unod az egyforma képeket..."
-    }
+    # # database request
+    # context = {
+    #     "site_name": "Stylus photography",
+    #     "site_subtitle": "Ha már unod az egyforma képeket..."
+    # }
 
-    return render(request, 'home.html', context)
+    # return render(request, 'home.html', context)
+    return render(request, 'home.html')
 
 
 def services_view(request):
-    # database request
-    context = {
-        "site_name": site_name,
-        "site_subtitle": "Ha már unod az egyforma képeket..."
-    }
+    # # database request
+    # context = {
+    #     "site_name": site_name,
+    #     "site_subtitle": "Ha már unod az egyforma képeket..."
+    # }
 
-    return render(request, 'services.html', context)
+    # return render(request, 'services.html', context)
+    return render(request, 'services.html')
 
 
 def about_view(request):
-    # database request
-    context = {
-        "site_name": site_name,
-        "site_subtitle": "Ha már unod az egyforma képeket..."
-    }
+    # # database request
+    # context = {
+    #     "site_name": site_name,
+    #     "site_subtitle": "Ha már unod az egyforma képeket..."
+    # }
 
-    return render(request, 'about.html', context)
+    # return render(request, 'about.html', context)
+    return render(request, 'about.html')
 
 
 def contact_view(request):
-    # database request
-    context = {
-        "site_name": site_name,
-        "site_subtitle": "Ha már unod az egyforma képeket..."
-    }
+    # # database request
+    # context = {
+    #     "site_name": site_name,
+    #     "site_subtitle": "Ha már unod az egyforma képeket..."
+    # }
 
-    return render(request, 'contact.html', context)
+    # return render(request, 'contact.html', context)
+    return render(request, 'contact.html')
 
 
 def gallery_view(request):
-    # database request
-    context = {
-        "site_name": site_name,
-        "site_subtitle": "Ha már unod az egyforma képeket..."
-    }
 
-    return render(request, 'gallery.html', context)
+    return render(request, 'gallery.html')
 
 
 
