7ae4e23acba0e2da9b768c15fc5eed6f49240f7a
ragnabt <rajhona.gabor@gmail.com>
2020-11-14 11:09:48 +0100

    site info, models.py

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -1,10 +1,20 @@
-
+from .models import SiteInfo
 
 def global_context(request):
+    site_infos = SiteInfo.objects.all()
+    # print(site_infos[0].site_name)
+    # print(site_infos[0].subtitle)
+    # print(site_infos[0].email)
+    # print(site_infos[0].phone)
+
+    if not site_infos:
+        return {}
+
     return {
-        "site_name": "Stylus photography",
-        "site_subtitle": "Ha már unod az egyforma képeket...",
-        "footer": "valaki@valami.com"
+        "site_name": site_infos[0].site_name,
+        "site_subtitle": site_infos[0].subtitle,
+        "email": site_infos[0].email,
+        "phone": site_infos[0].phone,
 
     }
 
--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class SiteInfo(models.Model):
+    site_name = models.CharField("Oldalnév", max_length=200)  # kötelezően kitöltendő mező
+    subtitle = models.CharField("Alcím", max_length=200)  # kötelezően kitöltendő mező
+    email = models.EmailField("Email", max_length=200, blank=True)  # opcionálisan kitöltendő mező
+    phone = models.CharField("Telefon", max_length=200, blank=True)  # opcionálisan kitöltendő mező
+
+    class Meta:
+        verbose_name_plural = "Oldal adatok"  # admin felületen a PAGES részben lévő adat neve ( lényegében a jelen osztály nevét írjuk így felül megjelenítéskor)
+
+    def __str__(self):
+        return self.site_name
+
