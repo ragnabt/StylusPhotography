ce0995ad5c878d6d41b948fd43abebffef5932c2
ragnabt <rajhona.gabor@gmail.com>
2020-11-19 20:27:36 +0100

    photo admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Category, Photo
 
 admin.site.register(Category)
-admin.site.register(Photo)
+
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ["title", "category", "uploaded", "image"]
+    list_editable = ["category"]
+    search_fields = ["title"]
+    list_filter = ["category"]
+
+
+admin.site.register(Photo, PhotoAdmin)
 
 
 
